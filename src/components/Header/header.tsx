c74befc56a3ca965ac82f3c626d66d80b1de96b4
LeonardoKamino <leo.kamino@gmail.com>
2025-02-27 10:43:22 -0800

    Update header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import "../../styles/header.scss";
 
@@ -6,16 +7,57 @@ const Header: React.FunctionComponent = () => {
   // Path to your resume PDF file
   const resumePdfPath = "/resume.pdf"; // Update this path
 
+  // Function to create stars and shooting stars
+  useEffect(() => {
+    const header = document.querySelector(".header");
+    if (header) {
+      // Create regular stars
+      for (let i = 0; i < 100; i++) {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.width = `${Math.random() * 4}px`;
+        star.style.height = star.style.width;
+        star.style.animationDelay = `${Math.random() * 2}s`;
+        header.appendChild(star);
+      }
+
+      // Create shooting stars
+      const createShootingStar = () => {
+        const shootingStar = document.createElement("div");
+        shootingStar.className = "shooting-star";
+        shootingStar.style.top = `${Math.random() * 100}%`;
+        shootingStar.style.left = `${Math.random() * 100}%`;
+        shootingStar.style.width = `${Math.random() * 5}px`;
+        shootingStar.style.height = shootingStar.style.width;
+        shootingStar.style.animationDuration = `${Math.random() * 7000 + 1000}ms`; // Random duration
+
+        // Generate a random angle between 0 and 360 degrees
+        const angle = Math.random() * 360;
+        shootingStar.style.setProperty("--angle", `${angle}deg`);
+
+        header.appendChild(shootingStar);
+
+        // Remove the shooting star after the animation ends
+        shootingStar.addEventListener("animationend", () => {
+          shootingStar.remove();
+        });
+      };
+
+      // Create shooting stars at random intervals
+      const shootingStarInterval = setInterval(createShootingStar, 500);
+
+      // Cleanup interval on component unmount
+      return () => clearInterval(shootingStarInterval);
+    }
+  }, []);
+
   return (
     <div className="header">
-      {/* Background Video */}
-      <video className="background-video" autoPlay loop muted>
-        <source src="/Background.mp4" type="video/mp4" />
-      </video>
-
       {/* Centered Content */}
-      <header className="viewport-header text-white">
-        <div className="my-32  xlg:my-64 mx-8">
+      <header className="viewport-header text-black dark:text-white">
+        <div className="my-32 xlg:my-64 mx-8">
           <h1 className="mb-12 text-5xl md:text-7xl xlg:text-8xl">
             Hey, I&apos;m Leo
           </h1>
