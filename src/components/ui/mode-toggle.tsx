e944a62fb4a797f1032dc2861fac916a72692d6a
LeonardoKamino <leo.kamino@gmail.com>
2025-03-14 01:12:24 -0700

    Update togle

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -3,21 +3,31 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 
-export default function ModeToggle() {
-  const { setTheme } = useTheme();
+interface ModeToggleProps {
+  className?: string;
+}
+
+export default function ModeToggle({ className }: ModeToggleProps) {
+  const { setTheme, theme } = useTheme();
 
   function toggleTheme() {
-    setTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"));
+    setTheme(theme === "light" ? "dark" : "light");
   }
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <div className={className}>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={toggleTheme}
+        className="rounded-full"
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </div>
   );
 }
